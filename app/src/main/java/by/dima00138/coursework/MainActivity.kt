f8b34d005199aa57180fa7718cbbe6b1bffa21c7
Timoshenko Dmitry <dima00138@gmail.com>
2024-03-19 21:26:25 +0300

    change app icon and add layout of navbar

--- app/src/main/java/by/dima00138/coursework/MainActivity.kt
+++ app/src/main/java/by/dima00138/coursework/MainActivity.kt
@@ -1,28 +1,39 @@
 package by.dima00138.coursework
 
+import android.graphics.drawable.Icon
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import by.dima00138.coursework.ui.theme.CourseWorkTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CourseWorkTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    BottomNavBar()
                 }
             }
         }
@@ -30,17 +41,25 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun BottomNavBar() {
+    var selectedItem by remember { mutableIntStateOf(0) }
+    val items = listOf("Search", "Board", "Orders", "Profile", "More")
+
+    NavigationBar {
+        items.forEachIndexed { index, item ->
+            NavigationBarItem(
+                selected = selectedItem == index,
+                onClick = { selectedItem = index },
+                label = { Text(item) },
+                icon = { /*TODO*/ })
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     CourseWorkTheme {
-        Greeting("Android")
+        BottomNavBar()
     }
 }
